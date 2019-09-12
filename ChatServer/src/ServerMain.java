c84c57cb5c60de026d68fcd35473d01ebc7dd879
Ivan Protsiuk <johnTranee@gmail.com>
2019-09-11 17:30:17 -0700

    test

--- ChatServer/src/ServerMain.java
+++ ChatServer/src/ServerMain.java
@@ -4,6 +4,6 @@ public class ServerMain {
     public static void main(String[] args) {
         int port = 8082; //set the port
         Server server = new Server(port); // create server instance
-        server.start(); // run run() in Server
+        server.start(); // run run() in Server.
     }
 }
